d9e952baf028956c3bc457f01f0f8d658e3a9d69
yashviordex123 <125557869+yashviordex123@users.noreply.github.com>
2023-07-29 00:03:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express=require('express')
+const app=express();
+const cors=require('cors')
+const PORT=6010
+require('./db/conn')
+const router=require('./Routes/router')
+
+// app.get('/',(req,resp)=>{
+//     resp.status(201).json("server start")
+// })
+
+app.use(cors());
+app.use(express.json())
+app.use(router);
+
+app.use('/uploads',express.static('./uploads'))
+app.use('/files',express.static('./public/files'))
+app.listen(PORT,()=>{
+    console.log(`Server start at port no ${PORT}`)
+})
